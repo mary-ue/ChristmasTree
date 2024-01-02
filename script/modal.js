2a7566152132bbe02902db2650863b9e5501d3ad
mary-ue <maryandreeva100@gmail.com>
2024-01-02 18:10:37 +0300

    add modal

---
+++ script/modal.js
@@ -0,0 +1,16 @@
+const buttonsOrder = document.querySelectorAll('.product__button_order');
+const overlayOrder = document.querySelector('.overlay_order');
+const order = overlayOrder.querySelector('.modal__order');
+
+buttonsOrder.forEach((buttonOrder) => {
+  buttonOrder.addEventListener('click', () => {
+    overlayOrder.classList.add('overlay_active');
+    order.value = buttonOrder.dataset.order;
+  })
+});
+
+overlayOrder.addEventListener('click', ({target}) => {
+  if (target === overlayOrder || target.closest('.modal__close')) {
+    overlayOrder.classList.remove('overlay_active');
+  }
+});
